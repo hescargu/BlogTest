fa21ee1d865ab1293ca3a4a1fca1dea4ff62e597
hescargu <kekedenfer@hotmail.com>
2011-11-24 23:04:40 +0100

    intergation create comment ok sauf  manque view

--- spec/requests/comment_create_spec.rb
+++ spec/requests/comment_create_spec.rb
@@ -3,37 +3,26 @@ require 'spec_helper'
 describe "CreateComments" do
   describe "GET /posts/:id" do
     before(:each) do
-      @post = stub_model(Post, :title => "sujet", :body => "cacahuete")
+      @post = Post.create(:title => "sujet", :body => "cacahuete")
       @posts = [@post]
-      visit post_path(@post.id)
     end
     it "displays a form to create a new comment" do
+      visit post_path(@post)
       page.should have_selector("commentform#new_comment")
-    end
-
-    it "should have a author field" do
       page.should have_field("Author")
-    end
-
-    it "should have a comment field" do
       page.should have_field("Comment")
-    end
-
-    it "should have a create comment button" do
       page.should have_button("Create Comment")
     end
   end
 
   describe "use new comment form" do
-    before(:each) do
-      @post = stub_model(Post, :title => "sujet", :body => "cacahuete")
+    it "should display the todo list" do
+      @post = Post.create(:title => "sujet", :body => "cacahuete")
       @posts = [@post]
       visit post_path(@post.id)
       fill_in("Author", :with => "auteur")
       fill_in("Comment", :with => "commentaire")
       click_button("Create Comment")
-    end
-    it "should display the todo list" do
       current_path.should == post_path(@post.id)
     end
     
@@ -41,8 +30,11 @@ describe "CreateComments" do
 
   describe "after a new comment has been created" do
     before(:each) do
-      @post = stub_model(Post, :title => "sujet", :body => "cacahuete")
+      @post = Post.create(:title => "sujet", :body => "cacahuete")
       @posts = [@post]
+    end
+
+    it "should display the new comment in the list" do
       visit posts_path
       within("tr", :text => @post.title) do
 	      click_link("Show Post")
@@ -52,9 +44,6 @@ describe "CreateComments" do
       fill_in("Comment", :with => "commentaire")
       click_button("Create Comment")
       current_path.should == post_path(@post.id)
-    end
-
-    it "should display the new comment in the list" do
       page.should have_content("auteur")
       page.should have_content("commentaire")
     end
