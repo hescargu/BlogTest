4b1dc9121c03a653f11d863b5466d133821d26ca
hescargu <kekedenfer@hotmail.com>
2011-11-24 22:51:43 +0100

    comment controller + spec ok

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,16 @@
 class CommentsController < ApplicationController
 	def create
-		@post = Post.find(:post_id)
+		puts params
+		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(params[:comment])
 		redirect_to post_path(@post), notice: 'Comment was successfully created.'
 	end
 	def destroy
+		puts params
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.find(params[:id])
 		@comment.destroy
-		redirect_to post_path(@post)
+		redirect_to post_path(@post), notice: 'Comment was successfully created.'
 	end
 end
 
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -22,6 +22,15 @@ describe CommentsController do
 			response.should redirect_to(post_path(@post))
 		end
 	end
+
+	def destroy
+		puts params
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+		redirect_to post_path(@post), notice: 'Comment was successfully created.'
+	end
+
 	describe "destroy" do
 		before(:each) do
 			@posts = [stub_model(Post,:title => "sujet1", :body => "rfhergzb"), 
@@ -30,6 +39,7 @@ describe CommentsController do
 			@post = @posts[0]
 			@comment = stub_model(Comment, :author => "auteur", :body => "commentaire", :post_id => @post.id)
 			@comments = [@comment]
+			#@params = {"id" => @comment.id, "post_id" => @post.id}
 			Post.stub(:find) { @post }
 			Post.stub(:comments) { true }
 			Comment.stub(:find) { @comment }
@@ -37,15 +47,13 @@ describe CommentsController do
 			visit post_path(@post)
 		end
 		it "delete doit avoir supprimer un comment" do
-			Post.should_receive(:find).and_return(@post)
+			Post.should_receive(:find).with(@post.id.to_s).and_return(@post)
 			@post.should_receive(:comments).and_return(@comments)
-			@comments.should_receive(:destroy).with(@new_comment["comment"]).and_return(@commentcreated)
-			post :create,  @new_comment
+			@comments.should_receive(:find).with(@comment.id.to_s).and_return(@comment)
+			@comment.should_receive(:destroy)
+			puts :post_id => @post.id, :id => @comment.id.to_i
+			delete :destroy, :post_id => @post.id, :id => @comment.id
 			response.should redirect_to(post_path(@post))
 		end
 	end
 end
-
-
-
-
